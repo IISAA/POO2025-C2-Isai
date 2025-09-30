a124f8521ebb2a534e5e1fc145660b95022b9395
IISAA <armutoisaidd@gmail.com>
2025-09-29 22:52:10 -0500

    Proyecto nueva rama

--- ConceptosPOO/src/main/java/pe/edu/upeu/conceptospoo/abspolimorfismo/Animal.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.conceptospoo.abspolimorfismo;
-
-public abstract class Animal {
-    public abstract void sonidoAnimal(); // solo está declarado
-
-    public void dormir(){ // está implmentando un método
-        System.out.println("Zzzzzzzzz");
-    }
-}
--- ConceptosPOO/src/main/java/pe/edu/upeu/conceptospoo/polimorfismo/Main.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.conceptospoo.polimorfismo;
-
-public class Main {
-    public static void main(String[] args) {
-        Loro loro = new Loro();
-        loro.sonidoAnimal();
-    }
-}
