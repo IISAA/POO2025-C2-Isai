50158d14724cc8e69c55165dd143473852694f9a
IISAA <armutoisaidd@gmail.com>
2025-10-07 13:03:42 -0500

    Segunda parte del SysVentas

---
+++ sysventas/src/main/java/pe/edu/upeu/sysventas/RunJavaFx.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.sysventas;
+
+public class RunJavaFx {
+    public static void main(String[] args) {
+        SysVentasApplication.main(args);
+        System.out.println("Hello, Mi name is ISAI");
+    }
+}
